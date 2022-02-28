f27046083d3707bb1ee002cf5c127ef642ca950a
Leo Kim <cloudkim2024@gmail.com>
2022-02-27 23:54:20 -0500

    React JSX reconstructing

--- straps/src/App.js
+++ straps/src/App.js
@@ -1,12 +1,29 @@
 import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+
 function App() {
+  let posts = ' 강남 고기 맛집 ';
+  let [titleState, titleModify] = useState('남자 코트 추천');  //스테이트 만드는법
+
+
   return (
     <div className="App">
-      <button type="button" class="btn btn-outline-primary">Primary</button>
+      <div className="">
+        <div class="black-nav">
+          Developer blog
+        </div>
+
+        <div class="list">
+          <h3> {titleState} </h3>
+          <p> 2월 17일 발행</p>
+        </div>
+
+      </div>
     </div>
   );
 }
 
 export default App;
+ 
