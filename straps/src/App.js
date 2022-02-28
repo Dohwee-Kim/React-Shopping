b6484e03f38b21c5ff91f7d5bec587aa1fdca3fb
Leo Kim <cloudkim2024@gmail.com>
2022-02-28 00:24:55 -0500

    React - re rendering

--- straps/src/App.js
+++ straps/src/App.js
@@ -5,18 +5,31 @@ import './App.css';
 
 function App() {
   let posts = ' 강남 고기 맛집 ';
-  let [titleState, titleModify] = useState('남자 코트 추천');  //스테이트 만드는법
-
+  let [titleState, titleModify] = useState(['남자 코트 추천','킹왕짱','넓은길댁']);  //스테이트 만드는법
+  let [thumbsUp, modifyThumbsUp] = useState(0);
 
   return (
     <div className="App">
       <div className="">
-        <div class="black-nav">
+        <div className="black-nav">
           Developer blog
         </div>
 
         <div class="list">
-          <h3> {titleState} </h3>
+          <h3>{titleState[0]} <span onClick={ ()=>{modifyThumbsUp(thumbsUp+1)}} >😀 </span> {thumbsUp}
+          <span onClick={ ()=>{titleModify(['여자 코트 추천','정말 짱','만만세'])}}> 😍 </span>
+          </h3>
+
+          <p> 2월 17일 발행</p>
+        </div>
+
+        <div class="list">
+          <h3> {titleState[1]} </h3>😀😃😍
+          <p> 2월 17일 발행</p>
+        </div>
+
+        <div class="list">
+          <h3> {titleState[2]} </h3>
           <p> 2월 17일 발행</p>
         </div>
 
