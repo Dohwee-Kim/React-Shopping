9f2b40029e89e39ff750bfe54dac7562e7d6ba8c
Leo Kim <cloudkim2024@gmail.com>
2022-03-01 14:33:33 -0500

    JumpOver mall initiate

--- straps/src/App.js
+++ straps/src/App.js
@@ -1,14 +1,62 @@
 import logo from './logo.svg';
 import React, {useState} from 'react';
+import {Nav, Navbar, Container, NavDropdown, Button} from 'react-bootstrap';
 import './App.css';
+
 
 
 function App() {
   return (
     <div>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Jump!Over</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    <JumboTronSample></JumboTronSample>
       
+      
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">dsdffs</div>
+          <div className="col-md-4">dsdffs</div>
+          <div className="col-md-4">dsdffs</div>
+        </div>
+      </div>
+
+
+
     </div>
   );
+}
+
+function JumboTronSample(){
+  return (
+    <div className="background">
+      <h1>20% Season off</h1>
+      <h1>Hello, Worlds!</h1>
+      <p>
+        This is a simple hero unit
+      </p>
+      <p>
+        <Button variant="primary">Learn more </Button>
+      </p>
+    </div>
+  )
 }
 
 export default App;
